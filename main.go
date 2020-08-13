9d8bdcbe90d8dec71457e2528db3cf53ac149c04
Fitiavana Ramanandafy <fitiavana.ramanandafy@gmail.com>
2020-08-13 22:32:51 +0300

    add sleep

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const (
@@ -13,8 +14,10 @@ const (
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
 
